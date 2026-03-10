431cdfc3478204f8ac0215026ea0d13c0de740ac
jun9191 <naightvor@gmail.com>
2026-03-10 16:58:32 +0900

    feat: dark theme, global styles, site constants

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const SITE = {
+  name: "buylow",
+  tagline: "zero to one",
+  description:
+    "맨손으로 시드를 만드는 실행부터, 자산을 지키고 불리는 통찰까지.",
+  subtitle: "가장 현실적인 부의 공략집",
+} as const;
+
+export const LINKS = {
+  naver_premium:
+    "https://contents.premium.naver.com/buylowh/buylow/contents",
+  youtube: "https://www.youtube.com/channel/UCIPq_gd8OkPYHlrmF30Lrpw",
+  instagram: "https://www.instagram.com/buylow_h/",
+} as const;
